efd1208d7e82c2eb80e03da83784bfc6e8d3f78b
FDC-Magno <60590518+FDC-Magno@users.noreply.github.com>
2021-11-07 21:17:38 +0800

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const path = require('path')
-const http = require('http')
 const socket = require('socket.io')
 const app = express()
 const PORT = process.env.PORT || 3000
